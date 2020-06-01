e3f9c82d521e0038fef0374df0c5aa0deb3c74b8
JebinK <jebink7@gmail.com>
2020-06-01 21:25:46 +0530

    reset button fixed

--- static/js/script.js
+++ static/js/script.js
@@ -1,11 +1,16 @@
 //challenge 1  age in days
+//clickme
 function ageIndays() {
     var birthyear = prompt("what year were you born?")
     var Indays = (2020 - birthyear) * 365
     var h1 = document.createElement("h1")
     var textanswer = document.createTextNode("you are "+ Indays + " days old")
-    h1.setAttribute("id", "Indays")
+    h1.setAttribute("id", "ageIndays")
     h1.appendChild(textanswer)
     document.getElementById("result").appendChild(h1)
-    
 }
+//reset
+function reset() {
+    document.getElementById("ageIndays").remove();
+}
+
