37f58e2da9c44dd8a143a3aff7c0610bc9da338c
JebinK <jebink7@gmail.com>
2020-06-01 21:17:47 +0530

    button clickme fixes

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,11 @@
 //challenge 1  age in days
+function ageIndays() {
+    var birthyear = prompt("what year were you born?")
+    var Indays = (2020 - birthyear) * 365
+    var h1 = document.createElement("h1")
+    var textanswer = document.createTextNode("you are "+ Indays + " days old")
+    h1.setAttribute("id", "Indays")
+    h1.appendChild(textanswer)
+    document.getElementById("result").appendChild(h1)
+    
+}
